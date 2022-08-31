a4eaa79c788db7607f2e74ac481829f9d614cf6f
abhi9065 <110813322+abhi9065@users.noreply.github.com>
2022-08-31 09:34:23 +0530

    done

--- app.js
+++ app.js
@@ -2,12 +2,34 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text){
+
+    return serverURL + "?" + "text=" + text
+
+}
+
+function errorHandler(error){
+    console.log("error occured",error)
+    alert("something went wrong the server please after some time ")
+
+}
+
 
 
 function clickhandler(){
-   
-    outputDiv.innerText = "ahahhaha" + txtInput.value
+    // outputDiv.innerText = "ahahhaha" + txtInput.value
 
-};
+    var inputText = txtInput.value;
+    
+    
+    fetch( getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
+    .catch(errorHandler)
+    
+    };
+
 
 btnTranslate.addEventListener("click", clickhandler);
